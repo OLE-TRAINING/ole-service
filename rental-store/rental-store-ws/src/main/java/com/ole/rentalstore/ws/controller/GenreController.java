e1e7486cbd3f985101332cdbe7aa2ee2f4647659
Bruno Monteiro <bsmonteiro99@gmail.com>
2018-11-25 12:27:30 -0200

    refactor completo

--- rental-store/rental-store-ws/src/main/java/com/ole/rentalstore/ws/controller/GenreController.java
+++ rental-store/rental-store-ws/src/main/java/com/ole/rentalstore/ws/controller/GenreController.java
@@ -3,13 +3,10 @@ package com.ole.rentalstore.ws.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ole.rentalstore.business.service.GenreService;
 import com.ole.rentalstore.commons.dto.tmdb_api.GenreResponseDTO;
-import com.ole.rentalstore.commons.dto.tmdb_api.MovieResponseDTO;
 
 @RestController
 public class GenreController {
@@ -20,12 +17,5 @@ public class GenreController {
 	@GetMapping("genres")
 	public ResponseEntity<GenreResponseDTO> getMovieGenres() {
 		return ResponseEntity.ok(genreService.getMovieGenres());
-	}
-
-	@GetMapping("genres/{id}/movies")
-	public ResponseEntity<MovieResponseDTO> getMoviesByGenre(@PathVariable("id") Integer id,
-															 @RequestParam("amount") Integer amount, 
-															 @RequestParam("page") Integer page) {
-		return ResponseEntity.ok(genreService.getMovies(id, page, amount, filter));
 	}
 }
